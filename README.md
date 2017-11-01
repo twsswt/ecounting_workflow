2d90bba798962f78f3ceafc88234a5d48c28ec78
twsswt <timothy.storer@glasgow.ac.uk>
2017-11-01 11:08:38 +0000

    Updated documentation.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# ecounting_workflow
-Socio-technical system model of the 2007 Scottish Elections Electronic Ballot Counting System.
+# E-vote Counting Workflows Models for Theatre_Ag/Fuzzi_Moss
+
+A model of the 2007 Scottish General Election Electronic Ballot Counting System in the Theatre_Ag/Fuzzi_Moss
+environment.
+
+## Contributors
+
+  * Tim Storer<br/>
+    School of Computing Science, University of Glasgow<br/>
+    GitHub ID: twsswt
+    [timothy.storer@glagow.ac.uk](mailto:timothy.storer@glagow.ac.uk)
+
+## References
+
---
+++ tests/ecount_system_test.py
@@ -0,0 +1,40 @@
+"""
+@author twsswt
+"""
+import unittest
+
+from mock import Mock
+
+from random import Random
+
+from ecounting_workflow.ecount_system import ElectoralReformServer, Vote, VoteDatabase
+
+
+class ElectoralReformServerTest(unittest.TestCase):
+
+    def setUp(self):
+        self.vote_database=Mock(spec=VoteDatabase)
+        self.electoral_reform_server = ElectoralReformServer(self.vote_database, 2)
+
+    def test_stv_gregory_method_elect_alice_then_bob(self):
+
+        test_votes = [
+            Vote(['Alice', 'Bob', 'Charlie']),
+            Vote(['Alice', 'Bob', 'Charlie']),
+            Vote(['Alice', 'Charlie', 'Bob']),
+            Vote(['Alice', 'Charlie', 'Bob'])
+        ]
+
+        self.vote_database.votes = set(test_votes)
+
+        random_mock = Mock(spec=Random)
+        random_mock.sample = Mock(side_effect=[
+            test_votes[0:2], test_votes[0:1]
+        ])
+
+        self.assertEquals({'Alice', 'Bob'}, set(self.electoral_reform_server.get_election_result(random_mock).keys()))
+
+
+
+if __name__ == '__main__':
+    unittest.main()
