2672162026a4b16da4384e235c26b3367f8017cb
twsswt <timothy.storer@glasgow.ac.uk>
2017-12-04 17:24:21 +0000

    Moved validation terminal to new module.

--- ecounting_workflow/count_hall.py
+++ ecounting_workflow/count_hall.py
@@ -12,14 +12,3 @@ class Shelf(object):
         pass
 
 
-class ValidationTerminal(object):
-
-    def __init__(self):
-        self.current_vote = None
-        self.current_vote_batch = None
-
-    def load_ballot_batch(self):
-        pass
-
-    def transition_ballot_batch(self):
-        pass
---
+++ ecounting_workflow/validation_terminal.py
@@ -0,0 +1,11 @@
+class ValidationTerminal(object):
+
+    def __init__(self):
+        self.current_vote = None
+        self.current_vote_batch = None
+
+    def load_ballot_batch(self):
+        pass
+
+    def transition_ballot_batch(self):
+        pass
